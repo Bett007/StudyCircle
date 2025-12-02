600a48cd73d82d1ee0c5e420da0e22e9ab418cbf
Bett007 <bettbrian022@gmail.com>
2025-12-02 23:56:36 +0300

    comments edit

--- Study_Circle/src/firebase.js
+++ Study_Circle/src/firebase.js
@@ -1,10 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+// web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "demo-api-key",
   authDomain: "demo-project.firebaseapp.com",
@@ -14,7 +11,7 @@ const firebaseConfig = {
   appId: "demo-app-id",
 };
 
-// Initialize Firebase
+// Initializing Firebase
 const app = initializeApp(firebaseConfig);
 
 // Initialize Cloud Firestore and get a reference to the service
